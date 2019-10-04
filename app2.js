da17a983b0ff32be1462705cd8f5123fc3b0f57c
Chris Smith <christopherkeithsmith@gmail.com>
2019-10-04 17:08:10 -0500

    complete!

--- app2.js
+++ app2.js
@@ -1,5 +1,11 @@
+// array of 9 cells from html
 const cells = document.querySelectorAll('.row > div');
+// selects h4 with id of banner
+const banner = document.querySelector('#banner');
+// variables for gameplay
 let currentPlayer = 'x';
+let isGameOver = false;
+let clickCounter = 0;
 
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', cellClicked);
@@ -8,18 +14,26 @@ for (let i = 0; i < cells.length; i++) {
 function cellClicked() {
     let cell = event.target;
 
+    // stops game from continuing if over and resets on click
+    if (isGameOver) {
+        resetGame();
+        return;
+    }
+
     // stops person from overriding a play
     if (cell.textContent === 'x' || cell.textContent === 'o') {
         return;
     }
 
-
     // draws player
     cell.textContent = currentPlayer;
+    // moves click counter up by 1 on each click
+    clickCounter++;
     checkWin();
     togglePlayer();
 }
 
+// toggles current player
 function togglePlayer() {
     if (currentPlayer === 'x') {
         currentPlayer = 'o';
@@ -30,35 +44,41 @@ function togglePlayer() {
 
 // checks each combination of cells for a win
 function checkWin() {
-    // x win conditions
-    // top row
-    if (cells[0].textContent === 'x' && cells[1].textContent === 'x' && cells[2].textContent === 'x') { alert('X Wins!') }
-    // middle row (horizontal)
-    else if (cells[3].textContent === 'x' && cells[4].textContent === 'x' && cells[5].textContent === 'x') { alert('X Wins!') }
-    // bottom row
-    else if (cells[6].textContent === 'x' && cells[7].textContent === 'x' && cells[8].textContent === 'x') { alert('X Wins!') }
-    // left row
-    else if (cells[0].textContent === 'x' && cells[3].textContent === 'x' && cells[6].textContent === 'x') { alert('X Wins!') }
-    // middle row (vertical)
-    else if (cells[1].textContent === 'x' && cells[4].textContent === 'x' && cells[7].textContent === 'x') { alert('X Wins!') }
-    // right row
-    else if (cells[2].textContent === 'x' && cells[5].textContent === 'x' && cells[8].textContent === 'x') { alert('X Wins!') }
-    else if (cells[0].textContent === 'x' && cells[4].textContent === 'x' && cells[8].textContent === 'x') { alert('X Wins!') }
-    else if (cells[2].textContent === 'x' && cells[4].textContent === 'x' && cells[6].textContent === 'x') { alert('X Wins!') }
+    // combinations of cells for a win
+    if (checkCells(0, 1, 2) || checkCells(3, 4, 5) || checkCells(6, 7, 8) || checkCells(0, 3, 6) || checkCells(1, 4, 7) || checkCells(2, 5, 8) || checkCells(0, 4, 8) || checkCells(2, 4, 6)) {
+        isGameOver = true;
+        banner.textContent = currentPlayer + ' wins!!! (click to reset board)';
+    }
 
-    // o win conditions
-    // top row
-    if (cells[0].textContent === 'o' && cells[1].textContent === 'o' && cells[2].textContent === 'o') { alert('O Wins!') }
-    // middle row (horizontal)
-    else if (cells[3].textContent === 'o' && cells[4].textContent === 'o' && cells[5].textContent === 'o') { alert('O Wins!') }
-    // bottom row
-    else if (cells[6].textContent === 'o' && cells[7].textContent === 'o' && cells[8].textContent === 'o') { alert('O Wins!') }
-    // left row
-    else if (cells[0].textContent === 'o' && cells[3].textContent === 'o' && cells[6].textContent === 'o') { alert('O Wins!') }
-    // middle row (vertical)
-    else if (cells[1].textContent === 'o' && cells[4].textContent === 'o' && cells[7].textContent === 'o') { alert('O Wins!') }
-    // right row
-    else if (cells[2].textContent === 'o' && cells[5].textContent === 'o' && cells[8].textContent === 'o') { alert('O Wins!') }
-    else if (cells[0].textContent === 'o' && cells[4].textContent === 'o' && cells[8].textContent === 'o') { alert('O Wins!') }
-    else if (cells[2].textContent === 'o' && cells[4].textContent === 'o' && cells[6].textContent === 'o') { alert('O Wins!') }
+    // tells game to register a draw if 9 moves have been made
+    else if (clickCounter === 9) {
+        isGameOver = true;
+        banner.textContent = 'draw!!! (click to reset board)';
+    }
+}
+
+// function that runs any number combinations through checkCells
+function checkCells(indexOne, indexTwo, indexThree) {
+    if (cells[indexOne].textContent === currentPlayer && cells[indexTwo].textContent === currentPlayer && cells[indexThree].textContent === currentPlayer) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// resets all variables back to initial values
+function resetGame() {
+    // loop through cells and clear textContent
+    for (let i = 0; i < cells.length; i++) {
+        // blank string to reset textContent
+        cells[i].textContent = '';
+    }
+    // reset gameOver to false for next playthrough
+    isGameOver = false;
+    // reset currentPlayer to 'x' for next playthrough
+    currentPlayer = 'x';
+    // reset clickCounter to 0
+    clickCounter = 0;
+    // reset banner to blank
+    banner.textContent = '';
 }
